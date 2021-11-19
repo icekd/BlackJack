1c02c73d64e3f51cff36c7f8747992a81e80afc5
icekd <32399403+icekd@users.noreply.github.com>
2021-11-19 08:29:04 -0800

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -2,7 +2,6 @@
 function shuffle(arr) {
   for(var i = arr.length - 1; i >= 0; i--) {
     var k = Math.floor((Math.random() * (i + 1)));
-    //console.log(k);
     var temp = arr[i];
     arr[i] = arr[k];
     arr[k] = temp;
@@ -17,19 +16,19 @@ const suits = [
 ];
 
 const faceCards = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-  "ten",
-  "joker",
-  "queen",
-  "king"    
+  ["one", 1],
+  ["two", 2],
+  ["three", 3],
+  ["four", 4],
+  ["five", 5],
+  ["six", 6],
+  ["seven", 7],
+  ["eight", 8],
+  ["nine", 9],
+  ["ten", 10],
+  ["joker", 11],
+  ["queen", 12],
+  ["king", 13]    
 ];
 
 var cards = [];
