f05ba63982ad7f0db89b832595450c003f117f74
icekd <32399403+icekd@users.noreply.github.com>
2021-11-19 21:53:47 -0800

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -8,8 +8,6 @@ function shuffle(arr) {
   } 
 }
 
-
-
 const suits = [
   "diamonds",
   "clubs",
@@ -51,24 +49,60 @@ let dealerCount = 0;
 let playerCount = 0;
 let index = 0;
 let bet = 0;
+function setHand(cards,dealerCount,playerCount,index) {
+    index = 0;
+    dealercount = 0;
+    playerCount = 0;
+    dealer.push(cards[index]);
+    player.push(cards[index + 1]);
+    dealer.push(cards[index + 2]);
+    player.push(cards[index + 3]);
+    index = index + 4;
+    dealerCount = dealerCount + cards[index].face[1] + cards[index + 2].face[1];
+    playerCount = playerCount + cards[index + 1].face[1] + cards[index + 3].face[1];
+   return {
+        dealerCount,
+        playerCount,
+        index
+    };
+}
 
-dealer.push(cards[index]);
-player.push(cards[index + 1]);
-dealer.push(cards[index + 2]);
-player.push(cards[index + 3]);
 
-dealerCount = dealerCount + cards[index].face[1] + cards[index + 2].face[1];
-playerCount = playerCount + cards[index + 1].face[1] + cards[index + 3].face[1];
 
-index = index + 4;
 
 
 console.log("Place Bet: \n");
 // user input will be defined based on end product framework such as Node.js, or html javascript
-bet = 1000;
+let bet = 1000;
+var coins = 10000;
+let userOption = 0;
 
+console.log("1. Hit 2. Stand: \n");
 while(game) {
-    
+    if(userOption == 1) {
+      player.push(cards[index]);
+      playerCount = playerCount + cards[index].face[1];
+      index = index + 1;
+    } else {
+        game = false;
+        if(playerCount > dealerCount) {
+           coins = coins + 2 * bet;
+           console.log("You won with a score of " + playerCount);
+        } else if(playerCount == dealerCount) {
+           coins = coins + bet;
+           console.log("You tied with a score of " + playerCount);
+        } else {
+           //coins are l
+           console.log("You lost with a score of " + playerCount);
+        }
+        game = false;
+        playerCount = 0;
+        dealerCount = 0;
+        dealer = [];
+        player = [];
+        bet = 0;
+        shuffle(cards);
+    }
 }
 
 
