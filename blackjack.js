72b9a3aa8eb8f0b01b8ed60014527236ccd46a8d
icekd <32399403+icekd@users.noreply.github.com>
2021-11-19 22:00:21 -0800

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -82,36 +82,37 @@ let bet = 1000;
 var coins = 10000;
 let userOption = 0;
 
-console.log("1. Hit 2. Stand: \n");
-while(game) {
-    if(userOption == 1) {
-      player.push(cards[index]);
-      playerCount = playerCount + cards[index].face[1];
-      index = index + 1;
-    } else {
-        game = false;
-        if(playerCount > dealerCount) {
-           coins = coins + 2 * bet;
-           console.log("You won with a score of " + playerCount);
-        } else if(playerCount == dealerCount) {
-           coins = coins + bet;
-           console.log("You tied with a score of " + playerCount);
+while(true) {
+    console.log("1. Hit 2. Stand: \n");
+    while(game) {
+        if(userOption == 1) {
+          player.push(cards[index]);
+          playerCount = playerCount + cards[index].face[1];
+          index = index + 1;
         } else {
-           //coins are l
-           console.log("You lost with a score of " + playerCount);
+            game = false;
+            if(playerCount > dealerCount) {
+               coins = coins + 2 * bet;
+               console.log("You won with a score of " + playerCount);
+            } else if(playerCount == dealerCount) {
+               coins = coins + bet;
+               console.log("You tied with a score of " + playerCount);
+            } else {
+               //coins are l
+               console.log("You lost with a score of " + playerCount);
+            }
+            game = false;
+            dealer = [];
+            player = [];
+            shuffle(cards);
+            let setup = setHand(cards);
+            let dealerCount = setup.dealerCount;
+            let playerCount = setup.playercount;
+            let index = setup.index;
+            let bet = setup.bet;
         }
-        game = false;
-        dealer = [];
-        player = [];
-        shuffle(cards);
-        let setup = setHand(cards);
-        let dealerCount = setup.dealerCount;
-        let playerCount = setup.playercount;
-        let index = setup.index;
-        let bet = setup.bet;
     }
 }
-
 
 
 
