20cd4a3d8c3132745d03db2d5df18335a2e36bc4
icekd <32399403+icekd@users.noreply.github.com>
2024-10-05 02:01:50 -0500

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -8,89 +8,119 @@ function shuffle(arr) {
   } 
 }
 
+// Helper function to calculate hand total with Ace handling
+function calculateHandTotal(hand) {
+  let total = 0;
+  let aceCount = 0;
+
+  for (let card of hand) {
+    total += card.face[1];
+    if (card.face[0] === "ace") {
+      aceCount++;
+    }
+  }
+
+  // Handle Aces, treating them as 1 if total goes over 21
+  while (total > 21 && aceCount > 0) {
+    total -= 10;  // Treat an Ace as 1 instead of 11
+    aceCount--;
+  }
+
+  return total;
+}
 
 function setHand(cards) {
   dealer.push(cards[index]);
   player.push(cards[index + 1]);
   dealer.push(cards[index + 2]);
   player.push(cards[index + 3]);
-  index = index + 4;
-  dealerCount = dealerCount + cards[index].face[1] + cards[index + 2].face[1];
-  playerCount = playerCount + cards[index + 1].face[1] + cards[index + 3].face[1];
- return {
-      dealerCount,
-      playerCount,
-      index,
-      bet
+  index += 4;
+
+  dealerCount = calculateHandTotal(dealer);
+  playerCount = calculateHandTotal(player);
+  
+  return {
+    dealerCount,
+    playerCount,
+    index,
+    bet
   };
 }
 
+function hitEvent() {
+  var img = document.createElement("img");
+  img.src = "cards/" + cards[index].face[0] + cards[index].suit + ".png"; 
+  img.height = 100;
+  img.width = 100;
 
-function hitEvent() {		
-      var img = document.createElement("img");
-      img.src = "cards/" + cards[index].face[0] + cards[index].suit + ".png"; 
-      
-      playerCount = playerCount  + cards[index].face[1];
-      
-      img.height = 100; 
-      img.width = 100;
-	    index = index + 1;
-      document.body.appendChild(img);
-      if(playerCount > 21) {
-        console.log("You lost with a score of " + playerCount);
-        endGame();
-     }
+  player.push(cards[index]);
+  playerCount = calculateHandTotal(player);
+  index++;
+
+  document.body.appendChild(img);
+  
+  if (playerCount > 21) {
+    console.log("You lost with a score of " + playerCount);
+    endGame();
+  }
 }
 
-
-
 function standEvent() {
+  // Dealer's turn
+  while (dealerCount < 17) {
+    dealer.push(cards[index]);
+    dealerCount = calculateHandTotal(dealer);
+    index++;
+  }
 
+  // Determine winner
+  if (dealerCount > 21) {
+    console.log("Dealer bust! You win!");
+  } else if (dealerCount > playerCount) {
+    console.log("Dealer wins with a score of " + dealerCount);
+  } else if (dealerCount < playerCount) {
+    console.log("You win with a score of " + playerCount);
+  } else {
+    console.log("It's a tie!");
+  }
+
+  endGame();
 }
 
 function startGame() {
-  document.getElementById('start').style.display= "none";
-  document.getElementById('hit').style.display="block";
-  document.getElementById('stand').style.display="block";
-
+  document.getElementById('start').style.display = "none";
+  document.getElementById('hit').style.display = "block";
+  document.getElementById('stand').style.display = "block";
+  
+  // Initialize hands and counts
+  shuffle(cards);
+  dealer = [];
+  player = [];
+  index = 0;
+  setHand(cards);
+  console.log("Dealer's first card: ", dealer[0].face[0] + " of " + dealer[0].suit);
+  console.log("Player's hand: ", player);
 }
 
 function endGame() {
-  let index = 0;
-  let dealerCount = 0;
-  let playerCount = 0;
-  let bet = 0;
-  document.getElementById('hit').style.display="none";
-  document.getElementById('stand').style.display="none";
+  dealer = [];
+  player = [];
+  dealerCount = 0;
+  playerCount = 0;
+  index = 0;
+  document.getElementById('hit').style.display = "none";
+  document.getElementById('stand').style.display = "none";
+  document.getElementById('start').style.display = "block";
 }
 
-
-
-const suits = [
-  "diamonds",
-  "clubs",
-  "hearts",
-  "spades"
-];
-
+const suits = ["diamonds", "clubs", "hearts", "spades"];
 const faceCards = [
-  ["ace", 11],
-  ["two", 2],
-  ["three", 3],
-  ["four", 4],
-  ["five", 5],
-  ["six", 6],
-  ["seven", 7],
-  ["eight", 8],
-  ["nine", 9],
-  ["ten", 10],
-  ["joker", 10],
-  ["queen", 10],
-  ["king", 10]    
+  ["ace", 11], ["two", 2], ["three", 3], ["four", 4],
+  ["five", 5], ["six", 6], ["seven", 7], ["eight", 8],
+  ["nine", 9], ["ten", 10], ["joker", 10], ["queen", 10], ["king", 10]
 ];
 
 var cards = [];
-
 for(let i = 0; i < suits.length; i++) {
   for(let j = 0; j < faceCards.length; j++) {
     cards.push({ face: faceCards[j], suit: suits[i] });
@@ -99,71 +129,9 @@ for(let i = 0; i < suits.length; i++) {
 
 shuffle(cards);
 
-//var game = true;
 var dealer = [];
 var player = [];
-
 var dealerCount = 0;
 var playerCount = 0;
-console.log(playerCount);
-console.log(playerCount);
-console.log(playerCount);
-console.log(playerCount);
-
 var index = 0;
 var bet = 100;
-
-
-//console.log("Place Bet: \n");
-// user input will be defined based on end product framework such as Node.js, or html javascript
-//bet = 1000;
-//var coins = 10000;
-//let userOption = 0;
-/*
-while(true) {
-    console.log("1. Hit 2. Stand: \n");
-    while(game) {
-        if(userOption == 1) {
-          player.push(cards[index]);
-          playerCount = playerCount + cards[index].face[1];
-          index = index + 1;
-          if(playerCount > 21) {
-            console.log("You got a bust with a score over 21");
-            game = false;
-            dealer = [];
-            player = [];
-            shuffle(cards);
-            setup = setHand(cards);
-            dealerCount = setup.dealerCount;
-            playerCount = setup.playercount;
-            index = setup.index;
-            bet = setup.bet;
-          }
-        } else {
-            if(playerCount > dealerCount) {
-               coins = coins + 2 * bet;
-               console.log("You won with a score of " + playerCount);
-            } else if(playerCount == dealerCount) {
-               coins = coins + bet;
-               console.log("You tied with a score of " + playerCount);
-            } else {
-               //coins are l
-               console.log("You lost with a score of " + playerCount);
-            }
-            game = false;
-            dealer = [];
-            player = [];
-            shuffle(cards);
-            setup = setHand(cards);
-            dealerCount = setup.dealerCount;
-            playerCount = setup.playercount;
-            index = setup.index;
-            bet = setup.bet;
-        }
-    }
-}
-*/
-
-
-
-
